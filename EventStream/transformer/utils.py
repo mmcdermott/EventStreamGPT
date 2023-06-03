f859347bf990288a8769a848e0c8a7fcbf1a63cc
Matthew McDermott <mattmcdermott8@gmail.com>
2023-06-03 09:26:27 -0500

    Added docstrings and doctests to transformers utils.

--- EventStream/transformer/utils.py
+++ EventStream/transformer/utils.py
@@ -130,23 +130,69 @@ def safe_masked_max(X: torch.Tensor, mask: torch.BoolTensor) -> torch.Tensor:
 
 
 def safe_weighted_avg(X: torch.Tensor, weights: torch.Tensor) -> tuple[torch.Tensor, torch.Tensor]:
-    """Produces a weighted average of the last dimension of `X`, weighted by the weights in
-    `weights` (which must be the same shape as `X`), except in the case where the sum of the weights
-    is 0, in which case the output returned is zero.
+    """Returns the weighted average of the last dimension of `X`, weighted by `weights`.
 
-    Also returns the sum of the weights. `weights` must be >= 0.
+    Args:
+        X: A tensor containing elements to take the weighted average over.
+        weights: The weights for the weighted average. Must be >= 0, and can take one of two forms:
+            * Element-wise, in which case it must have the same shape as `X`.
+            * Column-wise, in which case it must have the same shape as `X` _excluding the second to last
+                dimension, which should be omitted_, This case is used when you wish to, for example, take the
+                average of the hidden states of a network over the sequence length, while respecting an event
+                mask.
+
+
+    Returns:
+        For each index in the last dimension of `X`, returns a tuple containing:
+            * The weighted average of the last dimension of `X` weighted by `weights` for that index, unless
+                the weights for that index sum to 0, in which case the output returned is zero.
+            * The sum of the weights for that index (the denominator of the weighted average).
+
+    Raises:
+        AssertionError: If `weights` contains negative elements or has an invalid shape.
+
+    Examples:
+        >>> import torch
+        >>> X = torch.FloatTensor([[1, 2, 3], [4, 5, 6]])
+        >>> weights = torch.FloatTensor([[1, 2, 3], [4, 5, 6]])
+        >>> safe_weighted_avg(X, weights)
+        (tensor([2.3333, 5.1333]), tensor([ 6., 15.]))
+        >>> X = torch.FloatTensor([[1, 2, 3], [4, 5, 6]])
+        >>> weights = torch.FloatTensor([[0, 0, 0], [1, 0, 0]])
+        >>> safe_weighted_avg(X, weights)
+        (tensor([0., 4.]), tensor([0., 1.]))
+        >>> X = torch.FloatTensor([[1, 2, 3], [4, 5, 6]])
+        >>> weights = torch.FloatTensor([[0, 0, 0], [-1, 0, 0]])
+        >>> safe_weighted_avg(X, weights)
+        Traceback (most recent call last):
+            ...
+        AssertionError: weights should be >= 0
+        >>> X = torch.FloatTensor([[[1, 2, 3], [4, 5, 6]], [[7, 8, 9], [10, 11, 12]]])
+        >>> weights = torch.FloatTensor([[[1, 2], [3, 4], [5, 6]], [[7, 8], [9, 10], [11, 12]]])
+        >>> safe_weighted_avg(X, weights)
+        Traceback (most recent call last):
+            ...
+        AssertionError: weights torch.Size([2, 3, 2]) must be the same shape as X torch.Size([2, 2, 3])\
+ or the same shape as X excluding the second to last dimension
     """
+
     torch._assert(
         (weights >= 0).all(),
-        f"`weights` should be >= 0! Got {weights} with minimum {weights.min()}.",
+        "weights should be >= 0",
+    )
+
+    shape_err_string = (
+        f"weights {weights.shape} must be the same shape as X {X.shape} "
+        "or the same shape as X excluding the second to last dimension"
     )
 
     if len(weights.shape) < len(X.shape):
-        weights = weights.unsqueeze(-2).expand_as(X)
-
-    torch._assert(
-        weights.shape == X.shape, f"weights, {weights.shape} must be the same shape as X {X.shape}"
-    )
+        try:
+            weights = weights.unsqueeze(-2).expand_as(X)
+        except RuntimeError as e:
+            raise AssertionError(shape_err_string) from e
+    else:
+        torch._assert(weights.shape == X.shape, shape_err_string)
 
     denom = weights.float().sum(dim=-1)
     safe_denom = torch.where(denom > 0, denom, torch.ones_like(denom))
@@ -159,15 +205,34 @@ def safe_weighted_avg(X: torch.Tensor, weights: torch.Tensor) -> tuple[torch.Ten
 
 
 def weighted_loss(loss_per_event: torch.Tensor, event_mask: torch.Tensor) -> torch.Tensor:
-    """Given a tensor `loss_per_event` of shape [# subjects, # events] containing loss values per
+    """Returns the weighted average of the average per-event loss for each subject.
+
+    Given a tensor `loss_per_event` of shape [# subjects, # events] containing loss values per
     event per subject and a tensor `event_mask` containing binary indicators of whether any given
     event is present or not, returns the average per-subject of the average per-event loss for each
-    subject, excluding subjects who have no events."""
+    subject, excluding subjects who have no events.
+
+    Args:
+        loss_per_event: A tensor of shape [# subjects, # events] containing loss values per event
+        event_mask: A tensor of shape [# subjects, # events] containing binary indicators of whether an event
+            was present or not.
+
+    Returns:
+        A tensor of shape [] containing the weighted average of the average per-event loss for each subject,
+        excluding subjects who have no events. If no subjects have any events, returns 0.
+
+    Examples:
+        >>> import torch
+        >>> loss_per_event = torch.FloatTensor([[1, 2, 3], [4, 5, 6]])
+        >>> event_mask = torch.FloatTensor([[1, 1, 1], [1, 0, 0]])
+        >>> weighted_loss(loss_per_event, event_mask)
+        tensor(3.)
+    """
     loss_per_subject, events_per_subject = safe_weighted_avg(loss_per_event, event_mask)
     return safe_weighted_avg(loss_per_subject, (events_per_subject > 0))[0]
 
 
-_PROBS_LOGITS_NOT_BOTH_DISTRIBUTIONS = (
+_PROBS_LOGITS_NOT_BOTH_DISTRIBUTIONS: tuple[torch.distributions.Distribution] = (
     torch.distributions.Bernoulli,
     torch.distributions.Binomial,
     torch.distributions.Categorical,
@@ -181,13 +246,44 @@ def idx_distribution(
     D: torch.distributions.Distribution,
     index: INDEX_SELECT_T,
 ) -> torch.distributions.Distribution:
-    """Slices a torch Distribution so its outputs are of the appropriate shape. Sourced from:
-    https://github.com/pytorch/pytorch/issues/52625 on 2-16-22 and 12:40 ET.
+    """Slices a torch Distribution so its outputs are of the appropriate shape.
+
+    Torch distributions output tensors of consistent shape upon sample(). This method slices their internal
+    parameters so as to yield a new, transformed distribution whose outputs are sliced into a desired shape.
+    In many cases, but not all, if you use the slice/index you would use on an output sample() as the `index`
+    input to this method, the output distribution will have the desired shape.
+    Only works with select distributions.
+    Sourced from: https://github.com/pytorch/pytorch/issues/52625 on 2-16-22 at 12:40 ET.
 
     Args:
-        `D` (`torch.distributions.Distribution`): The distribution to slice.
-        `index` (`Union[INDEX_SELECT_T, Sequence[INDEX_SELECT_T]]`):
-            The index or slice to apply to the parameters.
+        D: The distribution to slice.
+        index: The index or slice to apply to the parameters.
+
+    Returns:
+        The sliced distribution.
+
+    Examples:
+        >>> import torch
+        >>> logits_tensor = torch.Tensor([[1, 2, -3], [4, 1, 0]])
+        >>> D = torch.distributions.Bernoulli(logits=logits_tensor)
+        >>> D.sample().shape
+        torch.Size([2, 3])
+        >>> D2 = idx_distribution(D, (slice(None), slice(None, 1)))
+        >>> D2.sample().shape
+        torch.Size([2, 1])
+        >>> D2.logits
+        tensor([[1.],
+                [4.]])
+        >>> probs_tensor = torch.FloatTensor([[0.1, 0.2, 0.7], [0.2, 0.8, 0.0]])
+        >>> D = torch.distributions.Categorical(probs=probs_tensor)
+        >>> D.sample().shape
+        torch.Size([2])
+        >>> D2 = idx_distribution(D, 1)
+        >>> D2.sample().shape
+        torch.Size([])
+        >>> # We have to round because distributions modify their probs params which yields precision errors
+        >>> D2.probs.round(decimals=1)
+        tensor([0.2000, 0.8000, 0.0000])
     """
     if not isinstance(index, tuple):
         index = (index,)
