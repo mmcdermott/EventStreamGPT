738f6e4b3adc82ef142cdb6c8ff46837c90a5df4
Matthew McDermott <mattmcdermott8@gmail.com>
2023-08-22 13:07:35 -0400

    Added the ability to test sample notebook tutorial code.

--- tests/data/test_sample_notebook.py
+++
@@ -1,9 +0,0 @@
-from pytest_notebook.nb_regression import NBRegressionFixture
-
-fixture = NBRegressionFixture(exec_timeout=50, exec_cwd="/home/mmd/Projects/EventStreamGPT/sample_data")
-fixture.diff_color_words = False
-fixture
-
-result = fixture.check(
-    "/home/mmd/Projects/EventStreamGPT/sample_data/examine_synthetic_data.ipynb", raise_errors=True
-)
