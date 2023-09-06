213797055990213d29ea879767f2607d304d0832
Matthew McDermott <mattmcdermott8@gmail.com>
2023-09-06 16:41:28 -0400

    Updated docs theme to use sphinx immaterial.

--- docs/conf.py
+++ docs/conf.py
@@ -86,6 +86,7 @@ extensions = [
     "myst_parser",
     "sphinxcontrib.collections",
     "nbsphinx",
+    "sphinx_immaterial",
 ]
 
 collections_dir = __location__ / "_collections"
@@ -186,7 +187,7 @@ todo_emit_warnings = True
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = "sphinx_material"
+html_theme = "sphinx_immaterial"
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
