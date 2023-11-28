241bfa12b1931bb8144e16c43c0fd5a95efd1d70
Matthew McDermott <mattmcdermott8@gmail.com>
2023-11-28 15:36:13 -0500

    pseudocode output layer

--- EventStream/transformer/conditionally_independent_model.py
+++ EventStream/transformer/conditionally_independent_model.py
@@ -21,7 +21,7 @@ from .transformer import (
 )
 
 
-class ConditionallyIndependentGenerativeOutputLayer(GenerativeOutputLayerBase):
+class ConditionallyIndependentGenerativeOutputLayer():
     """The output layer for the conditionally independent event stream model.
 
     TODO(mmcdermott):
@@ -38,15 +38,14 @@ class ConditionallyIndependentGenerativeOutputLayer(GenerativeOutputLayerBase):
         self,
         config: StructuredTransformerConfig,
     ):
-        super().__init__(config)
-        if config.structured_event_processing_mode != StructuredEventProcessingMode.CONDITIONALLY_INDEPENDENT:
-            raise ValueError(f"{config.structured_event_processing_mode} invalid!")
+        super().__init__()
+        self.rate_layer = torch.nn.Linear(config.hidden_size + config.query_hidden_size, 1)
 
     def forward(
         self,
-        batch: PytorchBatch,
         encoded: torch.FloatTensor,
-        is_generation: bool = False,
+        query_encoded: torch.FloatTensor,
+        answer: torch.FloatTensor,
     ) -> GenerativeSequenceModelOutput:
         """Returns the overall model output for the input batch.
 
@@ -62,103 +61,9 @@ class ConditionallyIndependentGenerativeOutputLayer(GenerativeOutputLayerBase):
                 predictoin back by one event in order to align with the labels.
         """
 
-        # These are the containers we'll use to process the outputs
-        classification_dists_by_measurement = {}
-        classification_losses_by_measurement = None if is_generation else {}
-        classification_labels_by_measurement = None if is_generation else {}
-        regression_dists = {}
-        regression_loss_values = None if is_generation else {}
-        regression_labels = None if is_generation else {}
-        regression_indices = None if is_generation else {}
-
-        classification_measurements = set(self.classification_mode_per_measurement.keys())
-        regression_measurements = set(
-            self.config.measurements_for(DataModality.MULTIVARIATE_REGRESSION)
-            + self.config.measurements_for(DataModality.UNIVARIATE_REGRESSION)
-        )
-
-        # encoded is of shape: (batch size, sequence length, config.hidden_size)
-        bsz, seq_len, _ = encoded.shape
-        whole_event_encoded = encoded
-
-        # In this case, the whole_event_encoded representation actually is used to predict the next event's
-        # contents, so it is what we want if we are in generative mode, but if we are not in generative mode
-        # then to make it align with the labels we need to shift it to be in the right form. In particular, we
-        # prepend a vector of zeros to be used to predict the contents of the first event (excluding the TTE
-        # of the first event which is guaranteed to be zero) and we _don't_ predict the contents of the event
-        # after the end of this sequence (as we have no way to judge them).
-
-        if is_generation:
-            for_event_contents_prediction = whole_event_encoded
-        else:
-            for_event_contents_prediction = torch.cat(
-                (
-                    torch.zeros_like(whole_event_encoded[:, 0, :]).unsqueeze(1),
-                    whole_event_encoded[:, :-1, :],
-                ),
-                dim=1,
-            )
-
-        classification_out = self.get_classification_outputs(
-            batch,
-            for_event_contents_prediction,
-            classification_measurements,
-        )
-        classification_dists_by_measurement.update(classification_out[1])
-        if not is_generation:
-            classification_losses_by_measurement.update(classification_out[0])
-            classification_labels_by_measurement.update(classification_out[2])
-
-        regression_out = self.get_regression_outputs(
-            batch,
-            for_event_contents_prediction,
-            regression_measurements,
-            is_generation=is_generation,
-        )
-        regression_dists.update(regression_out[1])
-        if not is_generation:
-            regression_loss_values.update(regression_out[0])
-            regression_labels.update(regression_out[2])
-            regression_indices.update(regression_out[3])
-
-        TTE_LL_overall, TTE_dist, TTE_true = self.get_TTE_outputs(
-            batch,
-            whole_event_encoded,
-            is_generation=is_generation,
-        )
-
-        return GenerativeSequenceModelOutput(
-            **{
-                "loss": (
-                    sum(classification_losses_by_measurement.values())
-                    + sum(regression_loss_values.values())
-                    - TTE_LL_overall
-                )
-                if not is_generation
-                else None,
-                "losses": GenerativeSequenceModelLosses(
-                    **{
-                        "classification": classification_losses_by_measurement,
-                        "regression": regression_loss_values,
-                        "time_to_event": None if is_generation else -TTE_LL_overall,
-                    }
-                ),
-                "preds": GenerativeSequenceModelPredictions(
-                    classification=classification_dists_by_measurement,
-                    regression=regression_dists,
-                    regression_indices=regression_indices,
-                    time_to_event=TTE_dist,
-                ),
-                "labels": GenerativeSequenceModelLabels(
-                    classification=classification_labels_by_measurement,
-                    regression=regression_labels,
-                    regression_indices=regression_indices,
-                    time_to_event=None if is_generation else TTE_true,
-                ),
-                "event_mask": batch["event_mask"],
-                "dynamic_values_mask": batch["dynamic_values_mask"],
-            }
-        )
+        predicted_rate = self.rate_layer(encoded + query_encoded)
+        return torch.distributions.Poisson(predicted_rate).pdf(answer)
+        
 
 
 class CIPPTForGenerativeSequenceModeling(StructuredGenerationMixin, StructuredTransformerPreTrainedModel):
